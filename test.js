8c43c7b91d2945d72a538c07f9072e29775fc068
avikdas2207 <avikdas3535@gmail.com>
2020-11-28 01:57:21 +0530

    Added Functionality to Vaidate Password with minimum 8 characters and minimum one upper case letter with Regex

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var readlineSync = require('readline-sync');
 
-let regex = /(^[0-9]\d{2}\s\d{10}$)/;
-    var PhoneNo = readlineSync.question("Enter Your Phone Number ");
-
-    if(regex.test(PhoneNo)){
-        console.log("Phone Number is Valid");
-    }else{
-        console.log("Phone Number is not Valid")
-    }
+let regex = /(?=.*[A-Z])[A-Za-z0-9]{8,15}$/;
+            var Pswd = readlineSync.question("Enter Your Password ", {hideEchoBack: true} );
+    
+            if(regex.test(Pswd)){
+                console.log("Password is Valid");
+            }else{
+                console.log("Password is not Valid")
+            }
--- utility.js
+++ utility.js
@@ -54,7 +54,7 @@ class UserRegistration{
 
         UserPswd = () => {
 
-            let regex = /[A-Za-z0-9]{8}/;
+            let regex = /(?=.*[A-Z])[A-Za-z0-9]{8,15}$/;
             var Pswd = readlineSync.question("Enter Your Password ", {hideEchoBack: true} );
     
             if(regex.test(Pswd)){
